5afd9a1697bda7e4abdc78e5d89227f09238055b
Shraddha Ravishankar <shraddha.ravishanker@version1.com>
2021-11-25 14:37:44 +0000

    added tf code for comp,subnet

---
+++ 03-data-sources.tf
@@ -0,0 +1,35 @@
+############################################################################
+# Data Sources:
+############################################################################
+
+# Tenancy:
+data "oci_identity_tenancy" "tenancy" {
+  tenancy_id = var.tenancy_ocid
+}
+
+# Tenancy Home Region:
+data "oci_identity_regions" "home-region" {
+  filter {
+    name   = "key"
+    values = [data.oci_identity_tenancy.tenancy.home_region_key]
+  }
+}
+
+# Tenancy Availability Domains:
+data "oci_identity_availability_domains" "ads" {
+  compartment_id = var.tenancy_ocid
+}
+
+# Object Storage Services:
+data "oci_core_services" "core_services" {
+  filter {
+    name   = "name"
+    values = ["All .* Services In Oracle Services Network"]
+    regex  = true
+  }
+}
+
+# Object Storage Namespace:
+data "oci_objectstorage_namespace" "namespace" {
+}
+
