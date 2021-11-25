08e6a0db56aa21d4e57d24d87c9aea37df3b53a7
Shraddha Ravishankar <shraddha.ravishanker@version1.com>
2021-11-25 14:34:31 +0000

    initial schema file changes

---
+++ 00-variables.tf
@@ -0,0 +1,35 @@
+############################################################################
+# Variables:
+############################################################################
+# Tenancy:
+
+variable "tenancy_ocid" {} 
+variable "region" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+
+##Label used as a prefix for naming resources
+variable customer_label{
+    default = ""
+}
+
+#Tags Name
+variable "StackName" {
+    default = ""
+}
+variable "StackOwner" {
+    default = ""
+}
+variable "ProjectName" {
+    default = ""
+}
+variable "BillingOwner" {
+    default = ""
+}
+variable "CostCenter" {
+    default = ""
+}
+variable "Workload" {
+    default = ""
+}
