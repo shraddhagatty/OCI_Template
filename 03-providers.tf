02d55b11d50a6b2a33bd34b9e8fde91c89e7e303
Shraddha Ravishankar <shraddha.ravishanker@version1.com>
2021-12-07 05:13:20 +0000

    variables

--- 03-providers.tf
+++ 03-providers.tf
@@ -15,9 +15,9 @@ terraform{
 provider "oci" {
   region           = var.region
   tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  #user_ocid        = var.user_ocid
+  #fingerprint      = var.fingerprint
+  #private_key_path = var.private_key_path
 }
 
 ############################################################################
